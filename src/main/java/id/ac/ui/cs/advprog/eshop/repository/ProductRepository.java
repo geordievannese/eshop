ff15c270cbe91dfd53be04f9950fefedeae74af8
Geordie Vannese <geordievannese01@gmail.com>
2025-02-27 20:07:53 +0700

    Implementing DIP

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -2,44 +2,48 @@ package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 @Repository
-public class ProductRepository {
+public class ProductRepository implements ProductRepositoryInterface {
     private List<Product> productData = new ArrayList<>();
 
+    @Override
     public Product create(Product product) {
         productData.add(product);
         return product;
     }
 
-    public Iterator<Product> findAll () {
+    @Override
+    public Iterator<Product> findAll() {
         return productData.iterator();
-
     }
-    public Product update(Product updatedProduct) {
+
+    @Override
+    public void delete(String productId) {
+        productData.removeIf(product -> product.getProductId().equals(productId));
+    }
+
+    @Override
+    public Product findid(String productId) {
         for (Product product : productData) {
-            if (product.getProductId().equals(updatedProduct.getProductId())) {
-                product.setProductName(updatedProduct.getProductName());
-                product.setProductQuantity(updatedProduct.getProductQuantity());
+            if (product.getProductId().equals(productId)) {
                 return product;
             }
-}
-        return null; // If product not found
+        }
+        return null;
     }
 
-    public boolean delete(String productId) {
-        Iterator<Product> iterator = productData.iterator();
-        while (iterator.hasNext()) {
-            Product product = iterator.next();
-            if (product.getProductId() != null && product.getProductId().equals(productId)) {
-                iterator.remove();
-                return true;  // Successfully deleted
+    @Override
+    public Product update(Product updatedProduct) {
+        for (int i = 0; i < productData.size(); i++) {
+            if (productData.get(i).getProductId().equals(updatedProduct.getProductId())) {
+                productData.set(i, updatedProduct);
+                return updatedProduct;
             }
-}
-        return false;  // Product not found
+        }
+        return null;
     }
 }
